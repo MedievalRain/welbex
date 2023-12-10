22044e24e8356380c463e0207fc2ee16c145e99d
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-10 22:49:15 +0300

    Basic header layout

---
+++ src/components/Contacts.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledContacts = styled.div``;
+
+function Contacts() {
+  return <StyledContacts>Contacts</StyledContacts>;
+}
+
+export { Contacts };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,29 @@
 import styled from "styled-components";
+import Logo from "../Logo";
+import { Contacts } from "./Contacts";
 
-const StyledHeader = styled.header``;
+const StyledHeader = styled.header`
+  background-color: transparent;
+`;
+
+const StyledNav = styled.nav`
+  display: flex;
+`;
+const RoutesWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
 
 function Header() {
-  return <StyledHeader>Header</StyledHeader>;
+  return (
+    <StyledHeader>
+      <StyledNav>
+        <Logo />
+        <RoutesWrapper>ROUTES</RoutesWrapper>
+        <Contacts />
+      </StyledNav>
+    </StyledHeader>
+  );
 }
 
 export { Header };
